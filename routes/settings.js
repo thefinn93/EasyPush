4407748748e931a2eaea7927af0a3cecf3822a5c
Finn <finn@finn.io>
2015-11-14 20:04:10 -0800

    Display title on the settings page

--- routes/settings.js
+++ routes/settings.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('settings');
+  res.render('settings', {'title': 'Settings'});
 });
 
 module.exports = router;
