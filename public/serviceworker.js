b8ffa6f1e6ad123dbf5766051623774bdbb07b4c
Finn <finn@finn.io>
2015-11-22 13:42:11 -0800

    Promise is an object, not a function

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -30,7 +30,7 @@ function makeNotification(data, registration) {
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
-  var windowOpen = Promise(function(resolve, reject) { resolve(); });
+  var windowOpen = new Promise(function(resolve, reject) { resolve(); });
   if(event.notification.data.url || event.notification.data.url !== null) {
     windowOpen = clients.openWindow(event.notification.data.url);
   }
