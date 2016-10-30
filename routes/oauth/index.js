a1a75c2fbf054c562b3c1dcf90b0a4420dbb3bbf
Finn <finn@finn.io>
2016-10-30 12:47:04 -0700

    Let's try python

--- routes/oauth/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var reddit = require('./reddit');
-
-router.get('/reddit', reddit.preauth);
-router.get('/reddit_callback', reddit.postauth);
-
-module.exports = router;
